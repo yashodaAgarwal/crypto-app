a0633005a09f352f2dc02350732af65cb7267825
yashodaAgarwal <55345522+yashodaAgarwal@users.noreply.github.com>
2022-07-06 03:03:29 +0530

    feat : Coin Page added

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import Banner from "./Banner/Banner";
 import Carousel from "./Carousel/Carousel";
+import CoinInfo from "./CoinInfo/CoinInfo";
 import CoinsTable from "./CoinsTable/CoinsTable";
 import Header from "./Header/Header";
 
 export {Header,
 Banner,
 Carousel,
-CoinsTable}
+CoinsTable,
+CoinInfo}
